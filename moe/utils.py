c0607e3250c6a0afc13257a7aa5dec12d44b96c6
Robert Dyro <rdyro@google.com>
2025-11-22 10:38:43 -0800

    custom gather plus pre-commit

--- moe/utils.py
+++ moe/utils.py
@@ -35,7 +35,6 @@ def empty(shape, dtype, out_sharding=None):
 
 @partial(jax.jit, static_argnames=("pad_size",))
 def padding_to_tile(sort_idx, group_sizes, pad_size: int = 128):
-  # first shorter description
   """Align a sort_idx groups to a multiple of the pad_size after the gather.
 
   The sort_idx is considered split into groups by the entries in group_sizes. This function modified sort_idx
@@ -65,3 +64,45 @@ def add_indices(idx_list: jax.Array, sizes: jax.Array, max_size: int, fill_value
   # the result is a list having sizes[0] of idx_list[0], sizes[1] of idx_list[1] and so on
   # the unfilled values are filled with fill_value
   return jnp.sum(idx_list[:, None] * mask, axis=0) + ~jnp.any(mask, axis=0) * fill_value
+
+
+def spread_arange_gather(total_length: jax.Array, counts: jax.Array, multiple: int = 4):
+  """Creates a dilated arange corresponding to group sizes (conuts) padded to `multiple`."""
+  new_counts = counts + (-counts % multiple)
+  offsets = (jnp.cumsum(new_counts) - new_counts) - (jnp.cumsum(counts) - counts)
+  starts = jnp.cumsum(new_counts) - new_counts
+  ends = starts + counts
+  arange = jnp.arange(total_length)[None, :]
+  mask = ((arange >= starts[:, None]) & (arange < ends[:, None]))
+  full = jnp.where(mask, (arange - offsets[:, None]), 0)
+  some_mask = jnp.any(mask, axis=0)
+  return jnp.where(some_mask, jnp.sum(full, 0), -1), some_mask
+
+
+def _padded_group_gather(x: jax.Array, idx: jax.Array, max_idx: int, multiple: int):
+  """Gather a tensor into groups according to group idx `idx`, but pad so groups are aligned to `multiple`."""
+  counts = jnp.bincount(idx, length=max_idx)
+  padding_idxs = add_indices(jnp.arange(max_idx), -counts % multiple, max_size=multiple - 1)
+  idx_with_padding = jnp.concat([idx, padding_idxs], axis=0)
+  gather_idx = jnp.argsort(idx_with_padding)
+  inv_gather_idx = jnp.argsort(gather_idx)
+  return (x[gather_idx, ...], inv_gather_idx), (idx, inv_gather_idx)
+
+
+@partial(jax.custom_vjp, nondiff_argnames=("max_idx", "multiple"))
+def padded_group_gather(x: jax.Array, idx: jax.Array, max_idx: int, multiple: int):
+  return _padded_group_gather(x, idx, max_idx=max_idx, multiple=multiple)[0]
+
+
+def padded_group_gather_fwd(x: jax.Array, idx: jax.Array, max_idx: int, multiple: int):
+  return _padded_group_gather(x, idx, max_idx=max_idx, multiple=multiple)
+
+
+def padded_gather_bwd(max_idx: int, multiple: int, res, g):
+  del max_idx, multiple
+  g = g[0]  # the other sensitivity term belongs to the indices
+  (idx, inv_gather_idx) = res
+  return g[inv_gather_idx[jnp.argsort(idx)], ...], None
+
+
+padded_group_gather.defvjp(padded_group_gather_fwd, padded_gather_bwd)
